3b5f0f9ea86930258eabe57f1b6536c0ed8abbe8
yairBR23-meet <109908955+yairBR23-meet@users.noreply.github.com>
2023-07-18 13:29:33 +0300

    Camera Working Commit
    you Lost the bet

---
+++ app/src/main/java/com/example/newas/MaleActivity.java
@@ -0,0 +1,24 @@
+package com.example.newas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MaleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_male);
+
+
+
+    }    public void onBackButtonClick(View view) {
+        // Handle the button click here
+        // For example, you can return to the MainActivity
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
