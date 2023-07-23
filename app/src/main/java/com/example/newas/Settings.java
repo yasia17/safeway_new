bd8a279369bdaf84fb4a3f9db73364524d3b68a5
pgltsmn <93178841+pgltsmn@users.noreply.github.com>
2023-07-23 11:57:21 +0300

    just frontend
    csdwdas

--- app/src/main/java/com/example/newas/Settings.java
+++ app/src/main/java/com/example/newas/Settings.java
@@ -2,14 +2,22 @@ package com.example.newas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.widget.Button;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+
 
 
 public class Settings extends AppCompatActivity {
@@ -19,14 +27,28 @@ public class Settings extends AppCompatActivity {
     private ImageButton nav;
     private ImageButton profile;
 
+    private ImageButton notifications;
+    private ImageButton aboutus;
+    private ImageButton logout;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        mAuth = FirebaseAuth.getInstance();
+
         settings = findViewById(R.id.SettingsBtn);
         nav = findViewById(R.id.NavBtn);
         profile = findViewById(R.id.ProfileBtn);
+        notifications = (findViewById(R.id.NotificationsBtn));
+        aboutus = findViewById(R.id.AboutBtn);
+        logout = findViewById(R.id.LogoutBtn);
+
+
+
 
 
         settings.setOnClickListener(new View.OnClickListener() {
@@ -52,5 +74,44 @@ public class Settings extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        aboutus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Settings.this, About_us.class);
+                startActivity(i);
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        
+    }
+
+    private void showLogoutConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to log out?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mAuth.signOut();
+                Intent i = new Intent(Settings.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
