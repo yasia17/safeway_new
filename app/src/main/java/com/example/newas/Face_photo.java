5870db381bbb50025aa3d0aab7001589b951a93a
pgltsmn <93178841+pgltsmn@users.noreply.github.com>
2024-01-24 23:00:32 +0200

    sssss

--- app/src/main/java/com/example/newas/Face_photo.java
+++ app/src/main/java/com/example/newas/Face_photo.java
@@ -16,6 +16,7 @@ public class Face_photo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face_photo);
 
+
         next_page = findViewById(R.id.next_page);
 
         next_page.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/newas/MainActivity.java
+++ app/src/main/java/com/example/newas/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Data_info.class);
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
                 startActivity(intent);
             }
         });
